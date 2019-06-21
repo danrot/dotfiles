6002ddec9cf89901457a1407734052850402a414
Daniel Rotter <daniel.rotter@gmail.com>
2019-06-21 18:05:18 +0200

    Adjust color of prompt to be easier recognizable

--- fish/functions.symlink/fish_prompt.fish
+++ fish/functions.symlink/fish_prompt.fish
@@ -1,8 +1,8 @@
 function fish_prompt
     set -l command_status $status
 
-    echo -n $USER@(prompt_hostname)
-    echo -n " "(set_color $fish_color_cwd)(prompt_pwd)(set_color normal)
+    echo -n (set_color -b cyan)(set_color black)$USER@(prompt_hostname)" "(set_color -b normal)
+    echo -n (set_color -b green)(set_color black)(prompt_pwd)(set_color -b normal)
 
     if [ $command_status -ne 0 ]
         set_color red
