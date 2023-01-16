67c9b972823c6733261123160652eb21af3e0d44
Daniel Rotter <daniel.rotter@gmail.com>
2023-01-16 09:44:56 +0100

    Add valantic specific paths

--- composer/config.fish
+++ composer/config.fish
@@ -1,1 +1,4 @@
 add_path_if_not_exists vendor/bin
+
+# valantic specifics
+add_path_if_not_exists project/vendor/bin
--- npm/config.fish
+++ npm/config.fish
@@ -1,2 +1,4 @@
 add_path_if_not_exists node_modules/.bin
+
+# valantic specifics
 add_path_if_not_exists assets/vue/node_modules/.bin
