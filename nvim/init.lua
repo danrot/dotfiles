0ef787b7b37953c0dec9dc60f68c5dbe4cd6712e
Daniel Rotter <daniel.rotter@gmail.com>
2023-03-06 15:43:01 +0100

    Use lualine git information from gitsigns to avoid failed process error caused by too many open files

--- nvim/init.lua
+++ nvim/init.lua
@@ -199,7 +199,26 @@ end
 
 require('Comment').setup()
 
+local function diff_source()
+	local gitsigns = vim.b.gitsigns_status_dict
+
+	if gitsigns then
+		return {
+			added = gitsigns.added,
+			modified = gitsigns.changed,
+			removed = gitsigns.removed
+		}
+	end
+end
+
 require('lualine').setup({
+	sections = {
+		lualine_b = {
+			{ 'b:gitsigns_head' },
+			{ 'diff', source = diff_source },
+			'diagnostics',
+		}
+	},
 	options = {
 		icons_enabled = false,
 		section_separators = '',
