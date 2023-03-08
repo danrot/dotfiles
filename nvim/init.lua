c39030557dcb690a355e20ee334a015a26f3ba7c
Daniel Rotter <daniel.rotter@gmail.com>
2023-03-08 13:41:22 +0100

    Ignore .git folder in nvim telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -231,7 +231,7 @@ local builtin = require('telescope.builtin')
 
 telescope.setup{
 	defaults = {
-		file_ignore_patterns = { 'vendor/.*/vendor' },
+		file_ignore_patterns = { '.git', 'vendor/.*/vendor' },
 		vimgrep_arguments = {
 			'rg',
 			'--color=never',
