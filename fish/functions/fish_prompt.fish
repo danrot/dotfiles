93e460d7d6d193a456fb393f0342f03e37b1d430
Daniel Rotter <daniel.rotter@gmail.com>
2023-05-24 13:27:39 +0200

    Configure kitty to make inactive windows recognizable by opaque text and adjust prompt to not use background color

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,8 +1,7 @@
 function fish_prompt
     set -l command_status $status
 
-    echo -n (set_color -b cyan)(set_color black)$USER@(prompt_hostname)" "(set_color -b normal)
-    echo -n (set_color -b green)(set_color black)(prompt_pwd)(set_color -b normal)
+    echo -n (set_color cyan)$USER@(prompt_hostname)" "(set_color green)(prompt_pwd)(set_color normal)
 
     if [ $command_status -ne 0 ]
         set_color red
