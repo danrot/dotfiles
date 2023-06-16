24aae36b5c1cdc153757540c1f8e28e07ba4da43
Daniel Rotter <daniel.rotter@gmail.com>
2023-06-16 11:46:18 +0200

    Remove valantic specifics

--- composer/config.fish
+++ composer/config.fish
@@ -1,4 +1,1 @@
 add_path_if_not_exists vendor/bin
-
-# valantic specifics
-add_path_if_not_exists project/vendor/bin
--- npm/config.fish
+++ npm/config.fish
@@ -1,4 +1,1 @@
 add_path_if_not_exists node_modules/.bin
-
-# valantic specifics
-add_path_if_not_exists assets/vue/node_modules/.bin
