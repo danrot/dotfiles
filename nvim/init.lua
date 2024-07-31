2849e6bcbe1fa61badeabd9b737ecad096c72d3f
Daniel Rotter <daniel.rotter@gmail.com>
2024-07-31 18:52:37 +0200

    Remove manual diagnostic keymap because it is included in neovim 0.11 by default

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,9 +73,6 @@ vim.diagnostic.config({
 	severity_sort = true,
 })
 
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-
 vim.cmd('colorscheme danrot')
 
 vim.api.nvim_create_autocmd('FileType', {
