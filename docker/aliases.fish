aa4f0f4106c8da6bf66e8d122d809ef8d63d8b93
Daniel Rotter <daniel.rotter@gmail.com>
2024-10-07 12:36:44 +0200

    Add alias to start docker compose up in watch mode

--- docker/aliases.fish
+++ docker/aliases.fish
@@ -1,6 +1,7 @@
 abbr --add d 'docker'
 abbr --add dc 'docker compose'
 abbr --add dcu 'docker compose up'
+abbr --add dcuw 'docker compose up --watch'
 abbr --add dce 'docker compose exec'
 abbr --add dcb 'docker compose build'
 abbr --add dr 'docker run'
